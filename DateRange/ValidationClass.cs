eb0399546dfdc9c61cae58bd2191c0803f431f07
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-01 17:00:48 +0300

    Create ValidationClass with constructor.

---
+++ DateRange/ValidationClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateRange
+{
+    class ValidationClass
+    {
+        private string startDate;
+        private string endDate;
+
+        public ValidationClass(string startDate, string endDate)
+        {
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+    }
+}
