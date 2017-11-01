5cfe4e9899fe6e2404c0a7d38cfb16821d3213e2
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-02 00:15:08 +0300

    Add StringToDateExtension with ConvertToDateTime method.

---
+++ DateRange/StringToDateTimeExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateRange
+{
+    public static class StringToDateTimeExtension
+    {
+        public static DateTime ConvertToDateTime(this string date)
+        {
+            var year = int.Parse(date.Substring(6));
+            var month = int.Parse(date.Substring(3, 2));
+            var day = int.Parse(date.Substring(0, 2));
+            return new DateTime(year, month, day);
+        }        
+    }
+}
