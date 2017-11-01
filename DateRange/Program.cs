1d8b3344ddb84c9a660e7570e6f268ed8b8e23d3
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-02 00:54:08 +0300

    Remove unused methods from Program.cs, Add instance of dateFormatter class into main method.

--- DateRange/Program.cs
+++ DateRange/Program.cs
@@ -9,28 +9,19 @@ namespace DateRange
     class Program
     {
         static void Main(string[] args)
-        {
+        {          
             try
             {
                 var startDate = args[0];
-                var endDate = args[1];                
-                Console.WriteLine(GetDateInterval(startDate, endDate));
+                var endDate = args[1];
+                var dateFormatter = new DateFormatter(startDate, endDate, new ValidationStatus());
+                Console.WriteLine(dateFormatter.ValidateAndGetRange(startDate, endDate));
             }
             catch (IndexOutOfRangeException)
             {
                 Console.WriteLine("No parameters have been passed.");
             }
-            Console.ReadLine();
-            
-        }
-
-        private static string GetDateInterval(string startDate, string endDate)
-        {
-            var validationStatus = new ValidationStatus(startDate, endDate);
-            if (validationStatus.IsValid())
-                return "IsValid";
-            return null;
-          
-        }
+            Console.ReadLine();            
+        }        
     }
 }
