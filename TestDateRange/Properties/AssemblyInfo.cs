4129ac0e894e4adbae515e77c75219df9da0115e
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-02 02:01:14 +0300

    Add TestDateRange project.

---
+++ TestDateRange/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestDateRange")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestDateRange")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c785610-9426-4cc9-ae00-4a2496a95370")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestDateRange/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DateRange;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDateRange
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestProperParametersWithDiffYearsPassedReturnProperValue()
+        {
+            var startDate = "01.01.2001";
+            var endDate = "02.02.2002";
+            var dateFormatter = new DateFormatter(startDate, endDate, new ValidationStatus());
+            var expected = "01.01.2001-02.02.2002";
+            Assert.AreEqual(expected, dateFormatter.ValidateAndGetRange());
+        }
+    }
+}
