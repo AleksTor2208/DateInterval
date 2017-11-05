e385fa8e8433570ea9e1a14d9df2531f5e08088a
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-05 04:45:25 +0300

    Add ArgsEncoder class with EncodeArgs method

---
+++ DateRange/ArgsEncoder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DateRange
+{
+    public class ArgsEncoder
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+        public void EncodeArgs(string[] args)
+        {
+            try
+            {
+                this.StartDate = args[0];
+                this.EndDate = args[1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Two arguments should be given.");
+            }
+        }
+    }
+}
