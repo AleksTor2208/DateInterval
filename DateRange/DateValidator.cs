1ba6b06d2b158214742528cbe7e65e73949ca453
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-07 02:18:22 +0300

    Refactor DateValidator class: Add IsConvertable, HasUsCultureFormat methods, modify IsCorrectDateOrder method

--- DateRange/DateValidator.cs
+++ DateRange/DateValidator.cs
@@ -5,53 +5,51 @@ namespace DateRange
 {
     public class DateValidator : IDateValidator
     {       
-        public string Message { get; set; }        
+        public string ErrorMessage { get; set; }
+        public string ErrorDetails { get; set; }
 
         public bool IsValid(string startDate, string endDate)
         {
-            return IsNotNull(startDate, endDate) && IsProperDateFormat(startDate, endDate) && IsCorrectDateOrder(startDate, endDate);
+            return IsConvertable(startDate, endDate)
+                && !HasUSCultureDateFormat(startDate)
+                && !HasUSCultureDateFormat(endDate)
+                && IsCorrectDateOrder(startDate, endDate);
+                
         }
 
-        private bool IsNotNull(string startDate, string endDate)
+        private bool HasUSCultureDateFormat(string date)
         {
-            const string incorrectFormatStatus = "Arguments can't be 'null'";
-            Message = incorrectFormatStatus;
-            return startDate != null && endDate != null;
-            
+            ErrorDetails = "Inapropriate yyyy.MM.dd format. Try dd.MM.yyy";
+            char[] separators = new char[] { '.', '/', ',', '_', '-'};            
+            const int yearMonthSeparatorIndex = 4;
+            const int monthDaysSeparatorIndex = 7;
+            var hasBadFormat = separators.Contains(date[yearMonthSeparatorIndex])
+                && separators.Contains(date[monthDaysSeparatorIndex]);
+            if (!hasBadFormat) return false;
+            ErrorMessage = ErrorDetails;
+            return true;                 
+        }
+
+        private bool IsConvertable(string startDate, string endDate)
+        {
+            ErrorDetails = $"{startDate}, {endDate} is not a valid date representation";
+            DateTime date;
+            var hasConverted = DateTime.TryParse(startDate, out date)
+                && DateTime.TryParse(endDate, out date);
+            if (hasConverted) return true;
+            ErrorMessage = ErrorDetails;
+            return false;            
         }
 
         private bool IsCorrectDateOrder(string startDate, string endDate)
         {
-            const string incorrectFormatStatus = "End date can not be less or same as first date";
-            var compareResult = DateTime.Compare(startDate.ConvertToDateTime(), endDate.ConvertToDateTime());
-            if (compareResult >= 0)
-            {
-                Message = incorrectFormatStatus;
-                return false;
-            }
-            return true;
-        }        
-
-        private bool IsProperDateFormat(string startDate, string endDate)
-        {
-            const string incorrectFormatStatus = "Incorrect date format, proper format is: dd.MM.yyyy";
-            var concatData = startDate + endDate;
-            concatData = concatData.Replace(".", "");
-            if (concatData.Any(t => !char.IsDigit(t)))
-            {
-                Message = incorrectFormatStatus;
-                return false;
-            }
-            if (CheckDots(startDate) && CheckDots(endDate)) return true;
-            Message = incorrectFormatStatus;
-            return false;
-        }
-
-        private static bool CheckDots(string date)
-        {
-            const int dayMonthSeparator = 2;
-            const int monthYearSeparator = 5;
-            return date[dayMonthSeparator] == '.' && date[monthYearSeparator] == '.';
-        }
+            ErrorDetails = "End date can not be less then first date";
+            var convStartDate = DateTime.Parse(startDate);
+            var convEtartDate = DateTime.Parse(endDate);
+            var compareResult = DateTime.Compare(convStartDate, convEtartDate);
+            if (compareResult < 0) return true;
+            ErrorMessage = ErrorDetails;
+            return false;                   
+        }          
     }
 }
