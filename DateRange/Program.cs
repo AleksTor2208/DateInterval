f65c669b65df3c8152400cc5c9c381264329867b
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-07 03:50:55 +0300

    Refactor Main method in Program.cs

--- DateRange/Program.cs
+++ DateRange/Program.cs
@@ -5,12 +5,13 @@ namespace DateRange
     public class Program
     {
         public static void Main(string[] args)
-        {            
-                var argsEncoder = new ArgsEncoder();
-                argsEncoder.EncodeArgs(args);
-                var dateFormatter = new DateFormatter(argsEncoder.StartDate, argsEncoder.EndDate, new DateValidator());
-                Console.WriteLine(dateFormatter.ValidateAndGetRange());
-                Console.ReadLine();
-        }        
+        {         
+            var argsEncoder = new ArgsEncoder();
+            argsEncoder.EncodeArgs(args);
+            var dateFormatter = new DateFormatter(argsEncoder.StartDate, argsEncoder.EndDate, new DateValidator());
+            var result = dateFormatter.ValidateAndGetRange();
+            Console.WriteLine(result);
+            Console.Read();
+        }
     }
 }
